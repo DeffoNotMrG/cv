e9ef80d61041473c3accbc974496d1fd82aef173
DeffoNotMrG <131198114+DeffoNotMrG@users.noreply.github.com>
2024-03-24 12:52:21 +0100

    1st commit
    Anne's cv

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,176 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Anne Nijland",
+  initials: "AN",
+  location: "Amersfoort, Netherlands",
+  locationLink: "https://goo.gl/maps/HcFhvcGZ4uvLZQwW6",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Hospitality Enthusiast",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "Dedicated and enthusiastic professional with a passion for hospitality and a genuine love for working with people. With a flexible mindset and a commitment to excellence, I thrive in dynamic environments. I am eager to contribute my skills and positive energy to a team committed to delivering exceptional service to all guests.",
+  avatarUrl: "https://media.licdn.com/dms/image/D5603AQFJ4GkgwujKQA/profile-displayphoto-shrink_800_800/0/1710343950800?e=1715817600&v=beta&t=C8vsrbZMhSlRAH2V2e1vA4bKhuVNZdhTiRKc55ULn_0",
+  personalWebsiteUrl: "https://annenijland.com/",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "anne.nijland@outlook.com",
+    tel: "+31 6 81688301",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/ghurmy",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/anne-n-288812168/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/ghurmy",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
-    },
+      school: "NHL Stenden University of Applied Sciences, Leeuwarden",
+      degree: "BA International Hospitality Management",
+      start: "2018",
+      end: "2022",
+      },
+      {
+      school: "HAN University of Applied Sciences, Nijmegen",
+      degree: "BA Sportkunde",
+      start: "2017",
+      end: "2018",
+      },
+      {
+      school: "Christelijk Lyceum Veenendaal",
+      degree: "HAVO, Economics & Society",
+      start: "2012",
+      end: "2017",
+      },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      company: "Van der Valk Amersfoort A1",
+      link: "/",
+      badges: ["Onsite"],
+      title: "Restaurant manager",
+      start: "April 2023",
+      end: "Present",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      "Managing day to day operations, responsible for 4 different F&B outlets, and in charge of hiring new staff.",
+      },
+      {
+      company: "Botanic Sanctuary Antwerp",
+      link: "/",
+      badges: ["Hybrid"],
+      title: "MICE Executive",
+      start: "July 2022",
+      end: "March 2023",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      "Organizing all sorts of events like congresses and large weddings. Performing administrative work including invoicing, quoting, and aftersales.",
+      },
+      {
+      company: "Botanic Sanctuary Antwerp",
+      link: "/",
+      badges: ["Hybrid"],
+      title: "Sales & Events Trainee",
+      start: "Sept 2021",
+      end: "June 2022",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
-    },
+      "Assisted with pre-opening operations, performed site visits, set up the reservations department of Hertog Jan at Botanic, and handled administrative work.",
+      },
+      {
+      company: "Barista Café Veenendaal",
+      link: "/",
+      badges: ["Onsite"],
+      title: "Barista & Hostess",
+      start: "April 2021",
+      end: "Aug 2021",
+      description:
+      "Obtained latte art skills and assisted with kitchen work.",
+      },
+      {
+      
+      company: "Van der Valk Leeuwarden",
+      link: "/",
+      badges: ["Onsite"],
+      title: "Restaurant Supervisor",
+      start: "Sept 2019",
+      end: "March 2021",
+      description:
+      "Managed day to day operations and was responsible for the Live Cooking restaurant with up to 400 seats.",
+      },
+      {
+      company: "Various",
+      link: "/",
+      badges: ["Onsite"],
+      title: "Various jobs in Hospitality",
+      start: "Feb 2016",
+      end: "Aug 2019",
+      description:
+      "Worked different jobs as a hostess, waitress and bartender in Veenendaal & Leeuwarden.",
+      },
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Communication",
+      "Leadership",
+      "Teamwork",
+      "Flexibility",
+      "Responsibility",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "SVH Leermeester",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "2023",
       ],
-      description: "A platform to build and grow your online business",
+      description: "",
       logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "Freelance",
+        href: "/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "BHV",
+      techStack: [
+        "2023",
+      ],
+      description: "",
+      logo: ConsultlyLogo,
+      link: {
+        label: "Freelance",
+        href: "https://www.ghurmy.xyz",
+      },
+    },
+    
+    {
+      title: "Wine Certificate SDEN1",
+      techStack: [
+        "2019",
+      ],
+      description: "",
+      logo: ConsultlyLogo,
+      link: {
+        label: "Freelance",
+        href: "/",
+      },
+    },
+    {
+      title: "Barista training by Single Estate",
+      techStack: ["2018",],
       description:
-        "Browser extension that records everything happening in a web application",
+        "",
       logo: MonitoLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "Freelancer",
+        href: "https://www.instagram.com/alshetkraakt/",
       },
     },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
+    
   ],
 } as const;
